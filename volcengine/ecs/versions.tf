69f83a757744dd000e57cd6dc352dec015865fd4
r0fus0d <d2hwakh7bs5e@protonmail.com>
2026-01-23 18:49:31 +0800

    更新火山引擎 ECS 版本至 1.0.1，并调整依赖项版本至 0.0.184

--- volcengine/ecs/versions.tf
+++ volcengine/ecs/versions.tf
@@ -1,18 +1,13 @@
 terraform {
   required_providers {
     volcengine = {
-      source  = "volcengine/volcengine"
-      version = "0.0.159"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
+      source = "volcengine/volcengine"
+      version = "0.0.184"
     }
   }
 }
 
 provider "volcengine" {
   region = "cn-beijing"
-  # access_key and secret_key can be provided via environment variables:
-  # VOLCENGINE_ACCESS_KEY and VOLCENGINE_SECRET_KEY
+  # Configuration options
 }
