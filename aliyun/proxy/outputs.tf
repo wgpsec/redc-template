d519b301e25fe86e33d7272eedac96b2002a51f0
r0fus0d <d2hwakh7bs5e@protonmail.com>
2026-03-08 22:22:04 +0800

    feat(proxy): 添加 ecs_password 输出并更新版本号

--- aliyun/proxy/outputs.tf
+++ aliyun/proxy/outputs.tf
@@ -2,3 +2,7 @@ output "ecs_ip" {
   value       = "${alicloud_instance.instance[*].public_ip}"
   description = "ip"
 }
+output "ecs_password" {
+  value       = nonsensitive(local.instance_password)
+  description = "vps password."
+}
